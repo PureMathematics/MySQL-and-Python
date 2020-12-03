7b2ffb2f5bfc3078722f59958c8ee267dce37489
David Lim <dlim46@gatech.edu>
2020-12-04 03:28:18 +0900

    backend changes

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,17 +1,18 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, render_template
 from database.db import initialize_db
-
-from resources.city import cities
+from flask_restful import Api
+from resources.routes import initialize_routes
 app = Flask(__name__)
-
+api = Api(app)
 app.config['MONGODB_SETTINGS'] = {
  'host': 'mongodb://localhost/city-list'
 }
 db = initialize_db(app)
-app.register_blueprint(cities)
+initialize_routes(api)
+
 
 #APIs
-@app.route("/")
+@app.route("/", methods=['GET'])
 def main():
     return render_template('index.html')
 
--- FlaskApp/resources/routes.py
+++ FlaskApp/resources/routes.py
@@ -1,5 +1,5 @@
 from .city import CitiesApi, CityApi
 
 def initialize_routes(api):
- api.add_resource(CitiesApi, '/cities')
- api.add_resource(CityApi, '/cities/<post_id>')
+    api.add_resource(CitiesApi, '/cities')
+    api.add_resource(CityApi, '/cities/<post_id>')
