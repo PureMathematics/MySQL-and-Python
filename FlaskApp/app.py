4cceddfda4d3a901c66b51f5beeb5d593d1de8ce
Eric Hsieh <ehsieh@res388d-128-61-95-83.res.gatech.edu>
2020-12-04 11:12:54 -0500

    Added more queries

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -36,6 +36,42 @@ with open('../json_db_lite.json') as json_file:
 		else:
 			contains_list.append("")
 
+# def get_query_main_tag_likes_comments(input1,input2, input3):
+# 	myquery = {"main_tag": input1, "likes":{ "$gt": input2 }"comments":{ "$gt": input3 }}
+# 	return mycol.find(myquery,{"im_640":1, "username":1, "main_tag":1, "likes":0, "comments":0, "locations":0, "contains":0, "post_id":0, "timestamp":0})
+
+# def get_query_main_tag_likes(input1, input2):
+# 	myquery = {"main_tag": input1, "likes":{ "$gt": input2 }}
+# 	return mycol.find(myquery,{"im_640":1, "username":1, "main_tag":1, "likes":0, "comments":0, "locations":0, "contains":0, "post_id":0, "timestamp":0})
+
+# def get_query_main_tag_comments(input1):
+# 	myquery = {"main_tag": input1, "comments":{ "$gt": input1 }}
+# 	return mycol.find(myquery,{"im_640":1, "username":1, "main_tag":1, "likes":0, "comments":0, "locations":0, "contains":0, "post_id":0, "timestamp":0})
+
+# def get_query_comments_likes(input1, input2):
+# 	myquery = {"likes": { "$gt": input2 }, "comments":{ "$gt": input2 }}
+# 	return mycol.find(myquery,{"im_640":1, "username":1, "main_tag":1, "likes":0, "comments":0, "locations":0, "contains":0, "post_id":0, "timestamp":0})
+
+# def get_query_main_tag(input1):
+# 	myquery = {"main_tag": input1}
+# 	return mycol.find(myquery,{"im_640":1, "username":1, "main_tag":1, "likes":0, "comments":0, "locations":0, "contains":0, "post_id":0, "timestamp":0})
+
+# def get_query_comments(input1):
+# 	myquery = {"comments" : { "$gt": input1 }}
+# 	return mycol.find(myquery,{"im_640":1, "username":1, "main_tag":1, "likes":0, "comments":0, "locations":0, "contains":0, "post_id":0, "timestamp":0})
+
+# def get_query_likes(input1):
+# 	myquery = {"likes" : { "$gt": input1 }}
+# 	return mycol.find(myquery,{"im_640":1, "username":1, "main_tag":1, "likes":0, "comments":0, "locations":0, "contains":0, "post_id":0, "timestamp":0})
+
+# def get_query_contains(input1):
+# 	myquery = {"contains:" input1}
+# 	return mycol.find(myquery,{"im_640":1, "username":1, "main_tag":1, "likes":0, "comments":0, "locations":0, "contains":0, "post_id":0, "timestamp":0})
+
+# def get_query_main_tag_contains(input1, input2):
+# 	myquery = {"contains": input1, "main_tag": input2}
+# 	return mycol.find(myquery,{"im_640":1, "username":1, "main_tag":1, "likes":0, "comments":0, "locations":0, "contains":0, "post_id":0, "timestamp":0})
+
 def get_main_tag_likes_comments_query(input1, input2, input3):
 	indices = []
 	for i, data in enumerate(likes_list):
@@ -56,8 +92,9 @@ def get_main_tag_likes_comments_query(input1, input2, input3):
 
 	indices3 = []
 	for i, data in enumerate(mlt2):
-		if data == input1:
-			indices3.append(i)
+		for k in input1:
+			if data == k:
+				indices3.append(i)
 	iml = [iml2[i] for i in indices3]
 	usl = [usl2[i] for i in indices3]
 	mlt = [mlt2[i] for i in indices3]
@@ -95,10 +132,12 @@ def get_main_tag_comments_query(input1, input2):
 	iml1 = [image_list[i] for i in indices]
 	usl1 = [username_list[i] for i in indices]
 	mlt1 = [main_tag_list[i] for i in indices]
+
 	indices2 = []
 	for i, data in enumerate(mlt1):
-		if data == input1:
-			indices2.append(i)
+		for k in input1:
+			if data == k:
+				indices2.append(i)
 	iml = [iml1[i] for i in indices2]
 	usl = [usl1[i] for i in indices2]
 	mlt = [mlt1[i] for i in indices2]
@@ -114,8 +153,9 @@ def get_main_tag_likes_query(input1, input2):
 	mlt1 = [main_tag_list[i] for i in indices]
 	indices2 = []
 	for i, data in enumerate(mlt1):
-		if data == input1:
-			indices2.append(i)
+		for k in input1:
+			if data == k:
+				indices2.append(i)
 	iml = [iml1[i] for i in indices2]
 	usl = [usl1[i] for i in indices2]
 	mlt = [mlt1[i] for i in indices2]
@@ -146,8 +186,9 @@ def get_likes_query(input):
 def get_main_tag_query(input):
 	indices = []
 	for i, data in enumerate(main_tag_list):
-		if data == input:
-			indices.append(i)
+		for k in input:
+			if data == k:
+				indices.append(i)
 
 	iml = [image_list[i] for i in indices]
 	usl = [username_list[i] for i in indices]
@@ -170,8 +211,9 @@ def get_contains_query(input):
 def get_main_tag_contains_query(input, ml, us, im):
 	indices = []
 	for i, data in enumerate(ml):
-		if data == input:
-			indices.append(i)
+		for k in input:
+			if data == k:
+				indices.append(i)
 
 	iml1 = [im[i] for i in indices]
 	usl1 = [us[i] for i in indices]
@@ -208,8 +250,10 @@ def querying():
 		min_likes = request.form['min_likes']
 	if request.form.get('min_comments'):
 		min_comments = request.form['min_comments']
+		print(min_comments)
 	if request.form.get('hashtag'):
 		main_tag = request.form['hashtag']
+		main_tag = main_tag.split(',')
 
 	if request.form.get('c_oneperson'):
 		contains.append(request.form['c_oneperson'])
@@ -282,7 +326,7 @@ def querying():
 		return render_template('displaying.html', ml = ml, us = us, im = im)	
 	
 	elif main_tag != "" and min_likes == 0 and min_comments != 0:
-		ml, us, im = get_main_tag_comments_query(main_tag, min_likes)
+		ml, us, im = get_main_tag_comments_query(main_tag, min_comments)
 		return render_template('displaying.html', ml = ml, us = us, im = im)		
 
 	elif main_tag != "" and min_likes != 0 and min_comments == 0:
